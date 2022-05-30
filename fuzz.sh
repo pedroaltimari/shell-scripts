39a7d450314dbd9b78c7efd36035c825444c5f33
Pedro Henrique Altimari Fontes <83617644+fontespedro@users.noreply.github.com>
2022-05-30 13:25:07 -0300

    Create fuzz.sh

---
+++ fuzz.sh
@@ -0,0 +1,18 @@
+###### SCRIPT NÃO FINALIZADO. PORÉM FUNCIONANDO.
+#!/bin/bash
+
+echo "-----> DEFINA SUA WORDLIST NO CÓDIGO ANTES DE INICIAR A FERRAMENTA <-----"
+echo "Digite o dominio: "
+read dominio;
+
+while read subd;
+do
+
+echo "Analisando..." $subd.$dominio
+echo "========================================================"
+host $subd.$dominio
+
+#if [[ $subdominio.$dominio -qe 1 ]]
+#then printf("\ 033 [0; 31m");
+
+done < wl.txt
